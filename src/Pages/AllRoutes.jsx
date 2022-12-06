ae683d2de31084966734bd109593bc9c733850d3
JERONIFAITHJ <jeronij10@gmail.com>
2022-12-06 21:15:18 +0530

    Implemented protected routing

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,22 +1,22 @@
-import React, { Fragment, Suspense } from "react";
+import React, { Fragment, Suspense, useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../Components/LandingPage/LandingPage";
 import Wrapper from "../Components/UI/Wrapper";
-// import AuthPage from "../Components/Auth/ExistingUser/AuthPage";
-// import TodoList from "../Components/MainContent/TodoList";
+import { UserInfoContext } from "../Components/Store/UserContext";
 const AuthPage = React.lazy(() =>
   import("../Components/Auth/ExistingUser/AuthPage")
 );
 const TodoList = React.lazy(() => import("../Components/MainContent/TodoList"));
 
 export default function AllRoutes() {
+  const [login] = useContext(UserInfoContext);
   const LoggedIn = () => {
-    if (true) {
+    if (login.isLoggedIn) {
       return <Outlet />;
     } else {
-      return <p>Failed</p>;
+      return <Navigate to='/auth' />; 
     }
   };
 
